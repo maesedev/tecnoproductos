58f29e71c43e60119881f7aff34c494514bdab92
santiago Garzon <79063950+maesedev@users.noreply.github.com>
2023-08-14 17:03:53 +0000

    Cambio de la variable de entorno de server_url

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -55,6 +55,7 @@ const Home: NextPage = (_props: InferGetServerSidePropsType<typeof getServerSide
           ),
         )}
       </div>
+      <a href="/experimental">exp</a>
     </section>
   );
 };
